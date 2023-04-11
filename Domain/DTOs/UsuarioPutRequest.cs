3e30cc87b88f9f324e4421ab3e84cbe549d97d4c
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-11 14:46:51 -0300

    add DTO's para usuario

---
+++ Domain/DTOs/UsuarioPutRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.DTOs
+{
+    public class UsuarioPutRequest
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Senha { get; set;}
+    }
+}
---
+++ Domain/DTOs/UsuarioRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.DTOs
+{
+    internal class UsuarioRequest
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Foto { get; set; }
+    }
+}
