95d4ae1bed9cec08e91d053e9a9a487af95fced1
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-11 22:27:48 -0300

    preparando para realizar pull da Development

--- Domain/DTOs/UsuarioRequest.cs
+++ Domain/DTOs/UsuarioRequest.cs
@@ -4,6 +4,7 @@
     {
         public string Nome { get; set; }
         public int Idade { get; set; }
+        public string Cpf { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
         public string Foto { get; set; }
