72d2110de2b1465c0cf98a22a190a1b61a6e64ac
nicolas ferreira <nicolas_f_pereira@estudante.sesisenai.org.br>
2023-04-12 14:51:13 -0300

    Adiciona DTOs dos cursos

---
+++ Domain/DTOs/CursosTelaInicialRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs
+{
+    internal class CursosTelaInicialRequest
+    {
+        public string Nome { get; set; }
+        public string Foto { get; set; }
+        public int categoria { get; set; }
+    }
+}
