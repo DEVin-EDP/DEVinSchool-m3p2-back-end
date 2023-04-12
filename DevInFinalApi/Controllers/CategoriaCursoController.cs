6dc6f179f894a5dbd6704365a6a5b278ef7347ce
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-12 20:30:34 -0300

    recriando migrations

--- DevInFinalApi/Controllers/CategoriaCursoController.cs
+++ DevInFinalApi/Controllers/CategoriaCursoController.cs
@@ -15,35 +15,31 @@ namespace DevInFinalApi.Controllers
         {
             _categoriaCursoService = categoriaCursoService;
         }
-        // GET: api/<CategoriaCursoController>
+
         [HttpGet]
         public async Task<ActionResult<dynamic>> GetCategoriaCurso()
         {
             return await _categoriaCursoService.GetCategoriaCurso();
         }
 
-        // GET api/<CategoriaCursoController>/5
         [HttpGet("{id}")]
         public async Task<ActionResult<dynamic>> GetCategoriaCurso(int id)
         {
             return await _categoriaCursoService.GetCategoriaCurso(id);
         }
-
-        // POST api/<CategoriaCursoController>
+        
         [HttpPost]
         public async Task<ActionResult<dynamic>> PostCategoriaCurso(CategoriaCursoRequestDto request)
         {
             return await _categoriaCursoService.PostCategoriaCurso(request);
         }
-
-        // PUT api/<CategoriaCursoController>/5
+        
         [HttpPut("update/{id}")]
         public async Task<ActionResult<dynamic>> PutCategoriaCurso(int id, CategoriaCursoRequestDto request)
         {
             return await _categoriaCursoService.PutCategoriaCurso(id, request);
         }
 
-        // DELETE api/<CategoriaCursoController>/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<dynamic>> DeleteCategoriaCurso(int id)
         {
