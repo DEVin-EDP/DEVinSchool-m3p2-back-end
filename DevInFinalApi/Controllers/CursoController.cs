73f4475e5223c6dbd07c218c6029c1ebf898a570
nicolas ferreira <nicolas_f_pereira@estudante.sesisenai.org.br>
2023-04-12 16:47:49 -0300

    Adicionado Controller

---
+++ DevInFinalApi/Controllers/CursoController.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Core;
+using Domain.Models;
+using Domain.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevInFinalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[Authorize]
+    public class CursoController : ControllerBase
+    {
+        private readonly ICursoService _CursoService;
+        public CursoController(ICursoService CursoService)
+        {
+            _CursoService = CursoService;
+        }
+
+        [HttpGet]
+        //[Authorize(Roles = ("usuario"))]
+        public async Task<ActionResult<dynamic>> GetCurso()
+        {
+            return await _CursoService.GetCurso();
+        }
+
+        [HttpGet("{id}")]
+        //[Authorize(Roles = ("usuario"))]
+        public async Task<ActionResult<dynamic>> GetCurso(int id)
+        {
+            return await _CursoService.GetCurso(id);
+        }
+
+        [HttpGet("categoria/{id}")]
+        //[Authorize(Roles = ("usuario"))]
+        public async Task<ActionResult<dynamic>> GetCursoCategoria(int id)
+        {
+            return await _CursoService.GetCursoCategoria(id);
+        }
+
+        [HttpPost]
+        //[Authorize(Roles = ("admin"))]
+        public async Task<ActionResult<dynamic>> PostCurso(Curso request)
+        {
+            return await _CursoService.PostCurso(request);
+        }
+
+        [HttpPut("{id}")]
+        //[Authorize(Roles = ("admin"))]
+        public async Task<ActionResult<dynamic>> PutCurso(int id, Curso request)
+        {
+            return await _CursoService.PutCurso(id, request);
+        }
+
+        [HttpDelete("{id}")]
+        //[Authorize(Roles = ("admin"))]
+        public async Task<ActionResult<dynamic>> DeleteCurso(int id)
+        {
+            return await _CursoService.DeleteCurso(id);
+        }
+    }
+}
