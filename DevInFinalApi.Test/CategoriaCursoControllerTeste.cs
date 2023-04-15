a2ac6422bcb7e57ed6432229a82f77f9c2dd4ec7
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-14 23:50:13 -0300

    Add Estrutura para testes na camada WebDenInFinalApi

---
+++ DevInFinalApi.Test/CategoriaCursoControllerTeste.cs
@@ -0,0 +1,16 @@
+namespace DevInFinalApi.Test
+{
+    public class CategoriaCursoControllerTeste
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ DevInFinalApi.Test/CursoSalvoControllerTeste.cs
@@ -0,0 +1,16 @@
+﻿namespace DevInFinalApi.Test
+{
+    internal class CursoSalvoControllerTeste
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
