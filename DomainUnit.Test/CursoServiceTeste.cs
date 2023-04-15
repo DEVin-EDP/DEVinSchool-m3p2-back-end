1433735651368ce1aadf512d98be521cc8e5ebe9
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-14 23:40:31 -0300

    Add estrutura para testes na camada Domain

---
+++ DomainUnit.Test/CursoServiceTeste.cs
@@ -0,0 +1,16 @@
+﻿namespace DomainUnit.Test
+{
+    internal class CursoServiceTeste
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ DomainUnit.Test/UsuarioServiceTeste.cs
@@ -0,0 +1,16 @@
+﻿namespace DomainUnit.Test
+{
+    internal class UsuarioServiceTeste
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
