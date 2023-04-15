ba6c72165642faae28f1c5297935bfe638b0876d
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-14 23:45:03 -0300

    Add estrutura para testes na camada Infrastructure

---
+++ Infrastructure.Test/CursoSalvoRepositoryTeste.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Test
+{
+    internal class CursoSalvoRepositoryTeste
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
