1f5b2b8baf72f29985f244e6ced3fb4bc6891857
leonardoGasperin <leonardovdg@hotmail.com>
2023-04-16 20:15:13 -0300

    EviaEmailServices testes

--- DevInFinalApi.Test/UsuarioControllerTeste.cs
+++ DevInFinalApi.Test/UsuarioControllerTeste.cs
@@ -11,7 +11,6 @@ namespace DevInFinalApi.Test
         [Test]
         public async Task GetUsuario_RetornaOkResult()
         {
-            // Arrange
             var mockUsuarioService = new Mock<IUsuarioService>();
             var controller = new UsuarioController(mockUsuarioService.Object);
             var usuario = new Usuario
@@ -30,10 +29,8 @@ namespace DevInFinalApi.Test
             var usuarios = new List<Usuario> { usuario };
             mockUsuarioService.Setup(service => service.GetUsuario()).ReturnsAsync(usuarios);
 
-            // Act
             var result = await controller.GetUsuario();
 
-            // Assert
             Assert.IsInstanceOf<OkObjectResult>(result.Result);
         }
     }
